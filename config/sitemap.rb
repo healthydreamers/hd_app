52ba087132c8cb47996b6c533279cc120d88af8b
Ismail Mechbal <ismail.mechbal@gmail.com>
2017-07-07 23:31:12 +0200

    Generate sitemap for published articles only

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,8 +1,8 @@
 SitemapGenerator::Sitemap.default_host = "https://healthydreamers.com"
 
 SitemapGenerator::Sitemap.create do
-  add root_path, :changefreq => 'daily'
-  Article.find_each do |article|
-    add article_path(article.slug), lastmod: article.updated_at
+  #add root_path, :changefreq => 'daily'
+  Article.where(articles: { is_published: true }).find_each do |article|
+		add article_path(article.slug), lastmod: article.updated_at
   end
 end
