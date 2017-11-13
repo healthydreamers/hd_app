b39af47530b450477f07f66c46e79907e5f6a84c
Ismail Mechball <ismail.mechbal@gmail.com>
2017-11-13 20:24:30 +0100

    Add namespace to redis to allow sidekiq to be used on another app

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-sidekiq_config = { url: ENV['REDIS_CACHE_URL'] }
+sidekiq_config = { url: ENV['REDIS_CACHE_URL'], namespace: 'hd_app_#{Rails.env}' }
 
 Sidekiq.configure_server do |config|
   config.redis = sidekiq_config
